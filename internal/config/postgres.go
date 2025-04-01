533ffdf699081e566a8544ad22e1fe843b0bcd53
ydoskeno <doskenovtar@gmail.com>
2025-04-01 07:03:24 +0500

    config added partly

--- internal/config/postgres.go
+++ internal/config/postgres.go
@@ -1,19 +1,23 @@
 package config
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 type PostgresConfig struct {
 	Host     string
-	Port     string
+	Port     int
 	Name     string
 	User     string
 	Password string
 }
 
 func NewConfigPostrgres() *PostgresConfig {
+	port, _ := strconv.Atoi("PG_PORT")
 	c := &PostgresConfig{
 		Host:     os.Getenv("PG_HOST"),
-		Port:     os.Getenv("PG_PORT"),
+		Port:     port,
 		Name:     os.Getenv("PG_NAME"),
 		User:     os.Getenv("PG_USER"),
 		Password: os.Getenv("PG_PASSWORD"),
--- internal/posgres/common.go
+++ internal/posgres/common.go
@@ -1,9 +1,25 @@
 package posgres
 
-import "1337b0rd/internal/config"
+import (
+	"1337b0rd/internal/config"
+	"1337b0rd/internal/posgres/auth"
+	"database/sql"
+	"fmt"
+)
 
-type Postgres struct{}
+type Postgres struct {
+	*auth.Auth
+}
 
-func NewPosgres(conf *config.PostgresConfig) (*Postgres, error) {
-	return nil, nil
+func NewDB(conf *config.PostgresConfig) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		conf.Host, conf.Port, conf.User, conf.Password, conf.Name,
+	)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
