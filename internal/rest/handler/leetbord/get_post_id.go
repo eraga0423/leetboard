c9b8a1780009ead85fdefc23c1872a1098abc4b8
eraga <eraga0423@gmail.com>
2025-04-11 11:47:12 +0500

    finish rest/get_post  Изменения, которые будут включены в коммит: 	изменено:      internal/rest/handler/leetbord/get_post_id.go

--- internal/rest/handler/leetbord/get_post_id.go
+++ internal/rest/handler/leetbord/get_post_id.go
@@ -4,20 +4,39 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
 	"text/template"
 
 	"1337b0rd/internal/constants"
 )
 
+type reqID struct {
+	id int
+}
+
 func (h *PostsHandler) GetPostID(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	tmpl := template.Must(template.ParseFiles(constants.Post))
 	id := r.PathValue("id")
-	
-	data := id // здесь будет governor
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		h.HandleError(w, http.StatusInternalServerError)
+		return
+	}
+	req := new(reqID)
+	req = &reqID{id: intID}
+	data, err := h.ctrl.OnePostGov(req, ctx)
+	if err != nil {
+		h.HandleError(w, http.StatusInternalServerError)
+		return
+	}
 	fmt.Println("This GET /post/id")
-	err := tmpl.Execute(w, data)
+	err = tmpl.Execute(w, data)
 	if err != nil {
 		slog.Any("fail", "error")
 		return
 	}
+}
+func (i *reqID) GetPostID() int {
+	return i.id
 }
