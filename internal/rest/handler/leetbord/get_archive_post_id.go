3e2485df7b88a084afa2099619459781f63f5285
eraga <eraga0423@gmail.com>
2025-04-30 16:06:52 +0500

    create rest post id  Changes to be committed: 	modified:   frontend/temp/archive_post.html 	modified:   frontend/temp/post.html 	modified:   internal/rest/handler/leetbord/get_archive_post_id.go 	modified:   internal/rest/handler/leetbord/get_post_id.go 	modified:   minio_data/.minio.sys/buckets/.bloomcycle.bin/xl.meta 	deleted:    minio_data/.minio.sys/buckets/.heal/mrf/list.bin 	modified:   minio_data/.minio.sys/buckets/.usage-cache.bin.bkp/xl.meta

--- internal/rest/handler/leetbord/get_archive_post_id.go
+++ internal/rest/handler/leetbord/get_archive_post_id.go
@@ -1,1 +1,144 @@
 package posts_handler
+
+import (
+	"log/slog"
+	"net/http"
+	"strconv"
+	"text/template"
+	"time"
+
+	"1337b0rd/internal/constants"
+	"1337b0rd/internal/types/controller"
+)
+
+type archiveReqID struct {
+	id int
+}
+
+type respOnePostArchive struct {
+	onePost  onePostArchive
+	comments []commentArchive
+}
+type commentArchive struct {
+	parentComment   oneCommentArchive
+	childrenComment []oneCommentArchive
+}
+
+type oneCommentArchive struct {
+	commentID      int
+	postID         int
+	author         authorArchive
+	commentContent string
+	commentImage   string
+	commentTime    time.Time
+}
+type authorArchive struct {
+	name      string
+	imageURL  string
+	sessionID string
+}
+type onePostArchive struct {
+	title      string
+	content    string
+	imageURL   string
+	postTime   time.Time
+	authorPost authorArchive
+}
+
+func (h *PostsHandler) GetPostIDArchive(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	tmpl := template.Must(template.ParseFiles(constants.Post))
+	id := r.PathValue("id")
+	intID, err := strconv.Atoi(id)
+	if err != nil {
+		h.HandleError(w, http.StatusInternalServerError)
+		return
+	}
+	var req archiveReqID
+	req = archiveReqID{
+		id: intID,
+	}
+
+	res, err := h.ctrl.OneArchivePostGov(&req, ctx)
+	if err != nil {
+		h.HandleError(w, http.StatusInternalServerError)
+		slog.Info("method", "gov", err)
+		return
+	}
+	comments := res.GetComments()
+	postResp := newArchivePost(res)
+	var newRespComment []commentArchive
+	for _, v := range comments {
+		parentResp := newArchiveParentComment(v.GetParent())
+		childResp := newArchiveChildrenComment(v.GetChildren())
+		newRespComment = append(newRespComment, commentArchive{
+			parentComment:   parentResp,
+			childrenComment: childResp,
+		})
+		respMain := respOnePostArchive{
+			onePost:  postResp,
+			comments: newRespComment,
+		}
+		err = tmpl.Execute(w, respMain)
+		if err != nil {
+			h.HandleError(w, http.StatusInternalServerError)
+			slog.Info("method", "front", err)
+			return
+		}
+	}
+}
+
+func (a *archiveReqID) GetPostID() int {
+	return a.id
+}
+
+func newArchivePost(req controller.ArchiveOnePostResp) onePostArchive {
+	respAuthPost := req.GetOnePost().GetAuthorPost()
+	respOnePost := req.GetOnePost()
+	newOnePost := onePostArchive{
+		title:    respOnePost.GetTitle(),
+		content:  respOnePost.GetPostContent(),
+		imageURL: respAuthPost.GetImageURL(),
+		postTime: respOnePost.GetPostTime(),
+		authorPost: authorArchive{
+			name:      respAuthPost.GetName(),
+			imageURL:  respAuthPost.GetImageURL(),
+			sessionID: respAuthPost.GetSessionID(),
+		},
+	}
+	return newOnePost
+}
+
+func newArchiveParentComment(parent controller.ArchiveOneComment) oneCommentArchive {
+	respComment := oneCommentArchive{
+		commentID: parent.GetCommentID(),
+		postID:    parent.GetPostID(),
+		author: authorArchive{
+			name:      parent.GetAuthor().GetName(),
+			imageURL:  parent.GetAuthor().GetImageURL(),
+			sessionID: parent.GetAuthor().GetSessionID(),
+		},
+		commentContent: parent.GetCommentContent(),
+		commentTime:    parent.GetCommentTime(),
+	}
+	return respComment
+}
+
+func newArchiveChildrenComment(child []controller.ArchiveOneComment) []oneCommentArchive {
+	newComment := make([]oneCommentArchive, len(child))
+	for _, comment := range child {
+		newComment = append(newComment, oneCommentArchive{
+			commentID: comment.GetCommentID(),
+			postID:    comment.GetPostID(),
+			author: authorArchive{
+				name:      comment.GetAuthor().GetName(),
+				imageURL:  comment.GetAuthor().GetImageURL(),
+				sessionID: comment.GetAuthor().GetSessionID(),
+			},
+			commentContent: comment.GetCommentContent(),
+			commentTime:    comment.GetCommentTime(),
+		},
+		)
+	}
+	return newComment
+}
--- internal/rest/handler/leetbord/get_post_id.go
+++ internal/rest/handler/leetbord/get_post_id.go
@@ -6,12 +6,44 @@ import (
 	"net/http"
 	"strconv"
 	"text/template"
+	"time"
 
 	"1337b0rd/internal/constants"
+	"1337b0rd/internal/types/controller"
 )
 
 type reqID struct {
 	id int
+}
+
+type RespOnePost struct {
+	OnePost  OnePost
+	Comments []Comment
+}
+type Comment struct {
+	ParentComment   OneComment
+	ChildrenComment []OneComment
+}
+
+type OneComment struct {
+	CommentID      int
+	PostID         int
+	Author         Author
+	CommentContent string
+	CommentImage   string
+	CommentTime    time.Time
+}
+type Author struct {
+	Name      string
+	ImageURL  string
+	SessionID string
+}
+type OnePost struct {
+	Title      string
+	Content    string
+	ImageURL   string
+	PostTime   time.Time
+	AuthorPost Author
 }
 
 func (h *PostsHandler) GetPostID(w http.ResponseWriter, r *http.Request) {
@@ -27,19 +59,83 @@ func (h *PostsHandler) GetPostID(w http.ResponseWriter, r *http.Request) {
 	req = &reqID{id: intID}
 	data, err := h.ctrl.OnePostGov(req, ctx)
 	if err != nil {
+		slog.Info("method", "gov", err)
 		h.HandleError(w, http.StatusInternalServerError)
 		return
 	}
-
-	slog.Info("data", data) /////////////////////////////////////раскрыть интерфейс ////////////////////////////
+	var newComment []Comment
+	newPost := newPost(data)
+	for _, v := range data.GetComments() {
+		newParent := newParentComment(v.GetParent())
+		newChild := newChildComments(v.GetChildren())
+		newComment = append(newComment, Comment{
+			ChildrenComment: newChild,
+			ParentComment:   newParent,
+		})
+	}
+	mainResp := RespOnePost{
+		OnePost:  newPost,
+		Comments: newComment,
+	}
 	fmt.Println("This GET /post/id")
-	err = tmpl.Execute(w, data)
+	err = tmpl.Execute(w, mainResp)
 	if err != nil {
-		slog.Any("fail", "error")
+		h.HandleError(w, http.StatusInternalServerError)
+		slog.Info("method", "front", err)
 		return
 	}
 }
 
 func (i *reqID) GetPostID() int {
 	return i.id
+}
+
+func newPost(req controller.OnePostResp) OnePost {
+	respAuthPost := req.GetOnePost().GetAuthorPost()
+	respOnePost := req.GetOnePost()
+	newOnePost := OnePost{
+		Title:    respOnePost.GetTitle(),
+		Content:  respOnePost.GetContent(),
+		ImageURL: respOnePost.GetImageURL(),
+		PostTime: respOnePost.GetPostTime(),
+		AuthorPost: Author{
+			Name:      respAuthPost.GetName(),
+			ImageURL:  respAuthPost.GetImageURL(),
+			SessionID: respAuthPost.GetSessionID(),
+		},
+	}
+	return newOnePost
+}
+
+func newParentComment(parent controller.OneComment) OneComment {
+	respComment := OneComment{
+		CommentID: parent.GetCommentID(),
+		PostID:    parent.GetPostID(),
+		Author: Author{
+			Name:      parent.GetAuthor().GetName(),
+			ImageURL:  parent.GetAuthor().GetImageURL(),
+			SessionID: parent.GetAuthor().GetSessionID(),
+		},
+		CommentContent: parent.GetCommentContent(),
+		CommentTime:    parent.GetCommentTime(),
+	}
+	return respComment
+}
+
+func newChildComments(child []controller.OneComment) []OneComment {
+	newComment := make([]OneComment, len(child))
+	for _, com := range child {
+		newComment = append(newComment, OneComment{
+			CommentID: com.GetCommentID(),
+			PostID:    com.GetPostID(),
+			Author: Author{
+				Name:      com.GetAuthor().GetName(),
+				ImageURL:  com.GetAuthor().GetImageURL(),
+				SessionID: com.GetAuthor().GetSessionID(),
+			},
+			CommentContent: com.GetCommentContent(),
+			CommentTime:    com.GetCommentTime(),
+		})
+	}
+	return newComment
 }
